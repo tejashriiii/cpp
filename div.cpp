42446ebb9b71b2185550f1f566b26f198a128aa6
tejashriiii <masketejashri11@gmail.com>
2025-10-24 16:17:02 +0530

    3

---
+++ div.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	double div;
+	cout << "give 2 numbers:" << endl;
+	cin >> a >> b;
+	div= (double)a/b;
+	cout << "dividing a and b:" << div;
+
+	return 0;
+}
