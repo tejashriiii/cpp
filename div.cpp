79b7175715841a73bc1c55d419219d9cbbd37b5a
tejashriiii <masketejashri11@gmail.com>
2025-10-24 16:22:11 +0530

    explain

--- div.cpp
+++ div.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-	int a,b;
+	int a,b; //this is beacuse if the user will give 1, 2 as values instead of  1.0 2.0 , the division wont happen, result of double needs double values to divide, if that makes sense
 	double div;
 	cout << "give 2 numbers:" << endl;
 	cin >> a >> b;
-	div= (double)a/b;
+	div= (double)a/b; //converting manually the int/int = int value we got into a double
 	cout << "dividing a and b:" << div;
 
 	return 0;
