2b382c4263329db65bf86e0ca7f41d534a75dda6
tejashriiii <masketejashri11@gmail.com>
2026-01-07 20:14:46 +0530

    array

---
+++ dsa/array.cpp
@@ -0,0 +1,13 @@
+// arrays.
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	// arrays should be of the same datatype
+	string cars[] = {"mustang ","ertiga ","tata ", "koenigsegg "};
+	for(int i=0; i<4;i++){
+		cout << cars[i]<< "\n";
+	}
+	return 0;
+}
